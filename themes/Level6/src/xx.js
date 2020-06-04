512637b56836e6bbbf50ed45c23f67505157fbf1
aphage <diyloli@outlook.com>
2020-06-04 21:09:50 +0800

    update

--- themes/Level6/src/xx.js
+++ themes/Level6/src/xx.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
-import 'social-share.js/dist/js/jquery.share.min.js'
+import * as social_share from './jquery.share.min.js'
+import './nprogress.css'
 
 export function init() {
 	$('.sidebar-nav li').click(function(e){
@@ -37,12 +38,12 @@ export function init() {
 	
 	function share () {
 	  if ($('.post-share').length) {
-		$('.post-share').share({
+		  social_share('.post-share',{
 		  disabled: ['tencent', 'douban', 'linkedin', 'diandian', 'facebook', 'google'],
 		  wechatQrcodeTitle: "微信扫一扫",
 		  wechatQrcodeHelper: '<p>微信扫一扫，右上角分享</p>',
 		  source: CONFIG.site.title
-		});
+		})
 	  }
 	}
 	share();
