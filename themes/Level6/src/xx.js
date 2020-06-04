b19de5459f5a5bc73c847fd1315d19d2797a4545
aphage <diyloli@outlook.com>
2020-06-04 21:31:26 +0800

    update

--- themes/Level6/src/xx.js
+++ themes/Level6/src/xx.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'
-require('./jquery.share.min.js')
+import * as sharex from './jquery.share.min.js'
 import './nprogress.css'
 
 export function init() {
@@ -38,7 +38,7 @@ export function init() {
 	
 	function share () {
 	  if ($('.post-share').length) {
-		socialShare('.post-share',{
+		sharex.socialShare('.post-share',{
 		  disabled: ['tencent', 'douban', 'linkedin', 'diandian', 'facebook', 'google'],
 		  wechatQrcodeTitle: "微信扫一扫",
 		  wechatQrcodeHelper: '<p>微信扫一扫，右上角分享</p>',
