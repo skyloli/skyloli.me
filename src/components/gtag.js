cda9e62bf024bb8eff31c847995693869de70a69
aphage <diyloli@outlook.com>
2021-02-13 22:15:29 +0800

    fix gtag.js

--- src/components/gtag.js
+++ src/components/gtag.js
@@ -2,16 +2,7 @@ import React from "react"
 
 const Gtag = () => {
     return (
-      <>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4EMLPCS7L0"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-4EMLPCS7L0');
-        </script>
-      </>
+      <script async src="/gtag.js"></script>
     )
 }
 
