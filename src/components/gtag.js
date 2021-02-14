37af1d256facfea277532094d37f4f42d269af6a
aphage <diyloli@outlook.com>
2021-02-14 21:59:53 +0800

    fix gtag.js

--- src/components/gtag.js
+++ src/components/gtag.js
@@ -1,22 +1,12 @@
-import { React, useEffect } from "react"
-import useScript from "../hooks/use-script"
-
-const gtag = () => {
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push(arguments)
-  }, [arguments])
-}
+import React from "react"
 
 const Gtag = () => {
-  useScript("https://www.googletagmanager.com/gtag/js?id=G-4EMLPCS7L0")
-  gtag('js', new Date());
-  gtag('config', 'G-4EMLPCS7L0');
-  console.log("alice foo foo");
-  return (
-    <div>
-    </div>
-  )
+    return (
+      <div>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4EMLPCS7L0"></script>
+        <script async src="/gtag.js"></script>
+      </div>
+    )
 }
 
 export default Gtag
